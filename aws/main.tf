6c4256a5362f4dec0f797b228959115b7479213b
Vitaliy <vitaliy.kostyukov@gmail.com>
2021-09-28 21:09:03 +0300

    add files azure/*

--- aws/main.tf
+++ aws/main.tf
@@ -1,11 +1,43 @@
 provider "aws" {
   region = "eu-central-1"
 }
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "example" {
   ami           = "ami-05f7491af5eef733a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  echo "Hello world" > index.html
+                  nohup busybox httpd -f -p ${var.server_port} &
+                  EOF
   tags = {
-    Name = "EC2-t2-micro-terraform"
+    Name = "terraform-example"
   }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the web server"
+  value       = aws_instance.example.public_ip
 }
---
+++ azure/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rgnorte" {
+  name     = "my-resources"
+  location = "West Europe"
+}
