c21c7c6c7ad0780ed0bbd95d2ff07f3290b3399c
Vitaliy <vitaliy.kostyukov@gmail.com>
2021-10-18 23:21:48 +0300

    change file aws/main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -7,7 +7,7 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
+/*resource "aws_instance" "example" {
   ami           = "ami-05f7491af5eef733a"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
@@ -20,7 +20,8 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
-}
+}*/
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -32,12 +33,128 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.example.id
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-05f7491af5eef733a"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  echo "Hello world" > index.html
+                  nohup busybox httpd -f -p ${var.server_port} &
+                  EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "instance_public_ip" {
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key   = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# ALB
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type  = "text/plain"
+      message_body  = "404: page not found"
+      status_code   = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn       = aws_lb_listener.http.arn
+  priority           = 100
+  condition {
+    path_pattern {
+      values         = ["*"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
+
+/*output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}*/
+
+/*output "instance_public_ip" {
   description = "The public IP address of the web server"
   value       = aws_instance.example.public_ip
-}
+}*/
