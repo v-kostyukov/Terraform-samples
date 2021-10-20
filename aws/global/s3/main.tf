31d5cda6320f63cb0fbbaed2476694e3c17b4ba4
Vitaliy <vitaliy.kostyukov@gmail.com>
2021-10-20 22:07:21 +0300

    add file aws/global/s3/main.tf

---
+++ aws/global/s3/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-state-2021"
+
+  # Предотвращаем случайное удаление бакета S3
+  lifecycle {
+    prevent_destroy = true
+  }
+  # Включаем управление версиями для просмотра всей истории файлов состояния
+  versioning {
+    enabled = true
+  }
+  # Включаем шифрование по умолчанию на стороне сервера
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-state-2021"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
